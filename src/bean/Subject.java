efe3baca0cc4c117ad954cc26c69b1b5cd296dda
Takahiro-ochiai-morioka <mro_b05@stu.o-hara.ac.jp>
2024-10-23 15:17:34 +0900

    Subjectクラスの完成

--- src/bean/Subject.java
+++ src/bean/Subject.java
@@ -4,28 +4,50 @@ import java.io.Serializable;
 
 public class Subject implements Serializable{
 
-	private String cd;
-	private String name;
-	private School school;
+	private String subjectId;
+	private String subjectCode;
+	private String subjectName;
+	private int courseYear;
+	private datetime regDate;
+	private datetime updateDate;
 
-	public String getCd() {
-		return cd;
-	}
-	public void setCd(String cd) {
-		this.cd = cd;
-	}
 
-	public String getName() {
-		return name;
+	public String getSubjectId() {
+		return subjectId;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public void setSubjectId(String subjectId) {
+		this.subjectId = subjectId;
 	}
 
-	public School getSchool() {
-		return school;
+	public String getSubjectCode() {
+		return subjectCode;
 	}
-	public void setSchool(School school) {
-		this.school = school;
+	public void setSubjectCode(String subjectCode) {
+		this.subjectCode = subjectCode;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+	public int getCourseYear(){
+		return  courseYear;
+	}
+	public void setRegDate(int courseYear) {
+		this.courseYear = courseYear;
+	}
+	public datetime getRegDate(){
+		return  regDate;
+	}
+	public void setRegDate(datetime regDate) {
+		this.regDate = regDate;
+	}
+	public datetime getUpdateDate(){
+		return  updateDate;
+	}
+	public void setUpdateDate(datetime updateDate) {
+		this.updateDate = updateDate;
 	}
 }

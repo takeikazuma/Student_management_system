835b30106410bc1ee375e247635944989e2725e3
djangouser <mtm_teacher_403@stu.o-hara.ac.jp>
2024-12-19 14:45:40 +0900

    test

--- src/bean/Absence.java
+++ src/bean/Absence.java
@@ -1,52 +1,71 @@
 package bean;
 
-import java.io.Serializable;
 import java.sql.Date;
 import java.time.ZonedDateTime;
 
-public class Absence implements Serializable{
+public class Absence {
+    private int gradeClassId;   // クラスID
+    private String className;    // クラス名
+    private int admissionYear;   // 入学年度
+    private Date startDate;      // 開始日
+    private Date endDate;        // 終了日
+    private ZonedDateTime regDate;   // 登録日時
+    private ZonedDateTime updateDate; // 更新日時
 
-	private Integer absenceId;	// DBに追加する際はnullなので、intではなくIntegerにする
-	private int gradeClassId;
-	private Date startDate;
-	private Date endDate;
-	private ZonedDateTime regDate;
-	private ZonedDateTime updateDate;
+    // Getter and Setter methods
+    public int getGradeClassId() {
+        return gradeClassId;
+    }
 
-	public Integer getAbsenceId() {
-		return absenceId;
-	}
-	public void setAbsenceId(Integer absenceId) {
-		this.absenceId = absenceId;
-	}
-	public int getGradeClassId() {
-		return gradeClassId;
-	}
-	public void setGradeClassId(int gradeClassId) {
-		this.gradeClassId = gradeClassId;
-	}
-	public Date getStartDate() {
-		return startDate;
-	}
-	public void setStartDate(Date startDate) {
-		this.startDate = startDate;
-	}
-	public Date getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	public ZonedDateTime getRegDate() {
-		return regDate;
-	}
-	public void setRegDate(ZonedDateTime regDate) {
-		this.regDate = regDate;
-	}
-	public ZonedDateTime getUpdateDate() {
-		return updateDate;
-	}
-	public void setUpdateDate(ZonedDateTime updateDate) {
-		this.updateDate = updateDate;
-	}
+    public void setGradeClassId(int gradeClassId) {
+        this.gradeClassId = gradeClassId;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getAdmissionYear() {
+        return admissionYear;
+    }
+
+    public void setAdmissionYear(int admissionYear) {
+        this.admissionYear = admissionYear;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public ZonedDateTime getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(ZonedDateTime regDate) {
+        this.regDate = regDate;
+    }
+
+    public ZonedDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(ZonedDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
 }
